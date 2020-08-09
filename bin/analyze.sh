93134b6c93f6777e99aa12d58cd16510dbfa641d
Andy Dunstall <andydunstall@hotmail.com>
2020-08-09 05:18:49 -0400

    Add bin todo

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Requires cpplint (pip3 install cpplint)
+# TODO(AD) use clang-tidy
 cpplint --recursive --exclude=lib/ --filter=-build/c++11,-whitespace/parens,-whitespace/line_length,-whitespace/braces .
