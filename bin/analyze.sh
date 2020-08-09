989e6d753411cbc3eb0e36507d61f694282ce341
Andy Dunstall <andydunstall@hotmail.com>
2020-08-09 07:02:15 -0400

    Remove bin

--- bin/analyze.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Requires cpplint (pip3 install cpplint)
-# TODO(AD) use clang-tidy
-cpplint --recursive --exclude=lib/ --filter=-build/c++11,-whitespace/parens,-whitespace/line_length,-whitespace/braces .
--- bin/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# C++
-find . -name "*.h" | xargs clang-format -i --style=Google
-find . -name "*.cc" | xargs clang-format -i --style=Google
-
-# Bazel
-buildifier -r .
